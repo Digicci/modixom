514ddfc41bc9bff8e2835b8f46d29c1ae4830c50
Brasero <brandonricci4@gmail.com>
2023-08-19 12:48:12 +0200

    maked the user page and conditionning the display with isPro or not

---
+++ src/pages/ConnectedRoot/Alerte/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {IonPage} from "@ionic/react";
+
+const Alerte: React.FC = () => {
+
+    return (
+        <IonPage>
+            Alertes
+        </IonPage>
+    );
+}
+
+export default Alerte;
