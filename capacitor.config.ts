ed2d62c8940d689e29b5287b5428caff1105299c
Brasero <brandonricci4@gmail.com>
2024-03-20 09:15:49 +0100

    adding appId in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'jkc.modixom.com',
   appName: 'modixom',
   webDir: 'dist',
   server: {
